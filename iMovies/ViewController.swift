8db8463ac80294addf19c6756e2caf0ffdb2e5a7
Yair Carreno <yaircarreno@gmail.com>
2018-04-01 10:23:01 -0500

    Initial Commit

---
+++ iMovies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iMovies
+//
+//  Created by Yair Ivan Carreño Lizarazo on 4/1/18.
+//  Copyright © 2018 qualityfull. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
